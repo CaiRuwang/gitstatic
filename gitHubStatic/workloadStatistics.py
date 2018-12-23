eff5150bd8556d4fcbf81dea51b9b15e57a1cae6
cairw <cairw@ebidding.com.cn>
2018-12-23 22:04:15 +0800

    提交统计代码

--- gitHubStatic/workloadStatistics.py
+++ gitHubStatic/workloadStatistics.py
@@ -6,7 +6,7 @@ import subprocess
 import ccbConstants as constants
 import UserWorkInfo as UserWorkInfo
 
-def commitInfo(filePath,sinceDate,endDate):
+def commitInfo(filePath,sinceDate,endDate,workInfoDic):
     #print filePath
     pNames = subprocess.Popen(
         "git log --format='%ae' | sort -u | while read name;do echo \"$name\t\";done",
@@ -20,15 +20,19 @@ def commitInfo(filePath,sinceDate,endDate):
             name = str(line)
             names.append(name)
 
-    workInfoDic={}
+
     for name in names:
         line=commitLineInfo(name,filePath,sinceDate,endDate)
         nums=commitNumsInfo(name,filePath,sinceDate,endDate)
         thisName=constants.emailConvert(name)
-        #print (thisName +'  '+line +',  commits: '+nums)
-        userWorkInfo=parseResult2Object(thisName +'  '+line +',  commits: '+nums)
-        print userWorkInfo.userName
+        resultStr= thisName +'  '+line +',  commits: '+nums
+        resultdo(workInfoDic,resultStr)
+        #userWorkInfo=parseResult2Object(resultStr)
+        #sprint userWorkInfo.userName
     pNames.wait()
+    print len(workInfoDic)
+
+
 
 # 函数用来统计 提交人 新增代码行数，删除代码行数，以及总行数
 def commitLineInfo(name,filePath,sinceDate,endDate):
@@ -65,14 +69,30 @@ def commitNumsInfo(name,filePath,sinceDate,endDate):
 def parseResultNums(sysOutStr):
     resultNums=0;
     strs=sysOutStr.split(":")
-    if(strs[1].isdigit()):
-        resultNums=strs[1]
+    if(is_number(strs[1])):
+        resultNums=int(strs[1])
     return resultNums;
 
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        pass
+
+    try:
+        import unicodedata
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
 # 将字符串转换成对象
 def parseResult2Object(resultStr):
     strs=resultStr.split(",");
-    userName= strs[0]
+    userName= userNameParse(strs[0])
     addLines=parseResultNums(strs[1])
     removedLines=parseResultNums(strs[2])
     totalLines=parseResultNums(strs[3])
@@ -80,9 +100,37 @@ def parseResult2Object(resultStr):
     userWorkInfo= UserWorkInfo.UserWorkInfo(userName,addLines,removedLines,totalLines,commits)
     return userWorkInfo
 
+def userNameParse(userName):
+    if(userName.isdigit()):
+        return  str(userName)
+    else:
+        return  userName
+
+
+#将结果集进行处理如果用户已经存在则将结果进行合并 如果结果不存在则放入字典
+def resultdo(workInfoDic,resultStr):
+    #print resultStr
+    userWorkInfo=parseResult2Object(resultStr)
+    if(workInfoDic.get(userWorkInfo.userName)):
+       oldUserWorkInfo= workInfoDic.get(userWorkInfo.userName)
+       addLines=oldUserWorkInfo.addLines+userWorkInfo.addLines
+       removedLines=oldUserWorkInfo.removedLines+userWorkInfo.removedLines
+       totalLines=oldUserWorkInfo.totalLines+userWorkInfo.totalLines
+       commits=oldUserWorkInfo.commits+userWorkInfo.commits
+       newUserWorkInfo=UserWorkInfo.UserWorkInfo(userWorkInfo.userName,addLines,removedLines,totalLines,commits)
+       workInfoDic.update({userWorkInfo.userName: newUserWorkInfo})
+    else:
+        workInfoDic.update({userWorkInfo.userName: userWorkInfo})
+
+
+
 
 
 if  __name__ == '__main__':
     # 考虑当gitHub有多个的情况时需要进行遍历多个git地址 故采用定义常量类定义路径
+    workInfoDic={}
     for filePath in  constants.filePaths():
-         commitInfo("/Users/cairuwang/java/code/ccb_v5/"+filePath,'2018-10-01','2018-11-21')
+         commitInfo("/Users/cairuwang/java/code/ccb_v5/"+filePath,'2018-10-01','2018-11-21',workInfoDic)
+    #print workInfoDic.items();
+    for key in workInfoDic:
+        print workInfoDic[key].userName +' addLines:'+str(workInfoDic[key].addLines)+' ,removedLineds:'+str(workInfoDic[key].removedLines)+' ,totalLines:'+str(workInfoDic[key].totalLines)+' ,commits: '+str(workInfoDic[key].commits)
